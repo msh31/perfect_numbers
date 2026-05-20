d93c5acb8ba877e57d3bd9e79f4351cf4356bbeb
Marco <marco@marco007.dev>
2026-05-20 19:33:48 +0200

    fix: use int64_t in is_prime & log time taken in microseconds per find

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <print>
 #include <cmath>
+#include <chrono>
 
-bool is_prime( int n ) {
+bool is_prime( int64_t n ) {
     if( n <= 1 ) return false;
-    for ( int i = 2; i <= sqrt( n ); i++ ) {
+    for ( int64_t i = 2; i <= sqrt( n ); i++ ) {
         if ( n % i == 0 ) return false;
     }
     return true;
@@ -12,12 +13,14 @@ bool is_prime( int n ) {
 int main( ) {
     std::println( "Hallo, Wereld!" );
 
+    auto start = std::chrono::high_resolution_clock::now();
     for ( int64_t i = 2; i <= INT64_MAX; i++ ) {
         if ( is_prime( std::pow( 2, i ) - 1 ) ) {
             int64_t perfect_number = ( std::pow( 2, i) - 1 );
             perfect_number *= std::pow( 2, i - 1 );
             if( perfect_number < 0 ) break;
-            std::println( "found a perfect number: {}", perfect_number );
+            auto stop = std::chrono::high_resolution_clock::now();
+            std::println( "found a perfect number: {} in {} microseconds", perfect_number, std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() );
         }
     }
 

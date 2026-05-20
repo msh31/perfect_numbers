9e46c2639d840a4d777a8de8693f4c0cfde921d7
Marco <marco@marco007.dev>
2026-05-20 18:53:33 +0200

    add is_prime function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <print>
+#include <cmath>
+#include <climits>
 
-int main() {
-    std::println("Hallo, Wereld!");
+bool is_prime( int n ) {
+    if( n <= 1 ) return false;
+    for ( int i = 2; i <= sqrt( n ); i++ ) {
+        if ( n % i == 0 ) return false;
+    }
+    return true;
+}
+
+int main( ) {
+    std::println( "Hallo, Wereld!" );
+
+    int n = INT_MAX;
+    std::println( "is {} a prime? - {}", n, is_prime( n ) );
     return 0;
 }

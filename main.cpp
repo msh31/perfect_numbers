de25ffee9e90ff3a81afca269f5dde14a4743185
Marco <marco@marco007.dev>
2026-05-20 19:24:08 +0200

    implement algorithm to find perfect numbers
    this only finds the first 5 because a regualr integer is too small

--- main.cpp
+++ main.cpp
@@ -14,6 +14,15 @@ int main( ) {
     std::println( "Hallo, Wereld!" );
 
     int n = INT_MAX;
-    std::println( "is {} a prime? - {}", n, is_prime( n ) );
+    for ( int i = 2; i <= INT_MAX; i++ ) {
+        if ( is_prime( std::pow( 2, i ) - 1 ) ) {
+            int perfect_number = ( std::pow( 2, i) - 1 );
+            perfect_number *= std::pow( 2, i - 1 );
+            if(perfect_number < 0) break;
+            std::println( "found a perfect number: {}", perfect_number );
+        }
+    }
+
+    // std::println( "is {} a prime? - {}", n, is_prime( n ) );
     return 0;
 }

bef47d16f98ccc7c372265332d2909f8467238c7
Marco <marco@marco007.dev>
2026-05-20 19:42:25 +0200

    swap int64_t for a BigInt - from a lib

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <cmath>
 #include <chrono>
 
-bool is_prime( int64_t n ) {
+#include "BigInt.hpp"
+
+bool is_prime( BigInt n ) {
     if( n <= 1 ) return false;
-    for ( int64_t i = 2; i <= sqrt( n ); i++ ) {
+    for ( BigInt i = 2; i <= sqrt( n ); i++ ) {
         if ( n % i == 0 ) return false;
     }
     return true;
@@ -14,13 +16,13 @@ int main( ) {
     std::println( "Hallo, Wereld!" );
 
     auto start = std::chrono::high_resolution_clock::now();
-    for ( int64_t i = 2; i <= INT64_MAX; i++ ) {
-        if ( is_prime( std::pow( 2, i ) - 1 ) ) {
-            int64_t perfect_number = ( std::pow( 2, i) - 1 );
-            perfect_number *= std::pow( 2, i - 1 );
+    for ( BigInt i = 2; i <= INT64_MAX; i++ ) {
+        if ( is_prime( pow( 2LL, i.to_long_long() ) - 1 ) ) {
+            BigInt perfect_number = ( pow( 2LL, i.to_long_long()) - 1 );
+            perfect_number *= pow( 2LL, i.to_long_long() - 1 );
             if( perfect_number < 0 ) break;
             auto stop = std::chrono::high_resolution_clock::now();
-            std::println( "found a perfect number: {} in {} microseconds", perfect_number, std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() );
+            std::println( "found a perfect number: {} in {} microseconds", perfect_number.to_string(), std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() );
         }
     }
 
